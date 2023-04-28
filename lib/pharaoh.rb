1a93ad64c5f692e651fb26662ad301a47c06f1a7
Melony Franchini <meltravelz@gmail.com>
2023-04-28 16:40:57 -0600

    add answer branch

--- lib/pharaoh.rb
+++ lib/pharaoh.rb
@@ -1,1 +1,46 @@
 # Create the Pharaoh class here 
+
+class Pharaoh
+	attr_reader :name,
+				:reputation,
+				:dynastic_period,
+				:phoenix
+
+	attr_accessor :age, :healthy
+
+	def initialize(name, reputation, dynastic_period, phoenix)
+		@name = name
+		@reputation = reputation
+		@dynastic_period = dynastic_period
+		@phoenix = phoenix
+		@healthy = true
+		@dead = false
+		@age = 0
+	end
+
+	def healthy?
+		@healthy
+	end
+
+	def ages
+		@age += 1
+		if @age >= 18
+			@healthy = false
+		end
+	end
+
+	def takes_action(emotion)
+		@phoenix.feels_emotion(emotion)
+	end
+
+	def dead?
+		@dead
+	end
+
+	def dies
+		#DOES this really make sense here? NO -> it should be in the Phoneix class
+		5.times { @phoenix.feels_emotion(:sorrow) }
+		@dead = true
+		@healthy = false
+	end
+end
--- lib/phoenix.rb
+++ lib/phoenix.rb
@@ -1,1 +1,66 @@
 # Create the Phoenix class here
+
+class Phoenix
+	attr_reader :name,
+							:color,
+							:mood,
+							:emotional_awareness,
+							:pharaoh
+
+	def initialize(name, color = "golden", mood = "stoic")
+		@name = name
+		@color = color
+		@mood = mood
+		@emotional_awareness = {}
+		@releases_tear = false
+		@pharaoh = nil
+	end
+
+	def bursts_into_flames
+			@color = "golden"
+			@mood = "stoic"
+			@pharaoh = nil
+			@emotional_awareness = {}
+			@releases_tear = false
+	end
+
+	def feels_emotion(emotion)
+		if @emotional_awareness.has_key?(emotion)
+			@emotional_awareness[emotion] += 1
+		
+			#pharaoh dies!
+			if @emotional_awareness[emotion] == 2
+				@color = "scarlet"
+				@mood = "fiery"
+			elsif @emotional_awareness[emotion] == 3
+				@color = "crimson"
+				@mood = "ablaze"
+				@releases_tear = true
+				@pharaoh.healthy = true if @pharaoh != nil
+					# the above line replaces this: 
+          # if @pharaoh != nil
+					# 	pharaoh.healthy = true 
+					# end
+			elsif @emotional_awareness[emotion] == 4
+				@releases_tear = false
+				@color = "deep violet"
+				@mood = "incandescent"
+			elsif @emotional_awareness[emotion] >= 5
+				bursts_into_flames
+			end
+
+		else
+			@emotional_awareness[emotion] = 1
+			@color = "amber"
+			@mood = "heated"
+		end
+	end
+
+	def releases_tear?
+		@releases_tear
+	end
+
+	def follows_pharaoh(pharaoh)
+		@pharaoh = pharaoh
+	end
+end
